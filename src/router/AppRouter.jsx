d3d87db19d251e8702c522fd2ffcc801fe1e3a06
ÇETİN <cetinnnarasss@gmail.com>
2022-07-19 21:31:32 +0300

    cont.

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,18 +1,20 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "../pages/Main";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
-import MovieDetail from "../pages/MovieDetail";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import Login from '../pages/Login';
+import Main from '../pages/Main';
+import MovieDetail from '../pages/MovieDetail';
+import Register from '../pages/Register';
+
 const AppRouter = () => {
   return (
     <BrowserRouter>
-    <Navbar/>
-          <Routes>
+      <Navbar />
+      <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/detail/:id" element={<MovieDetail />} />
+        <Route path="/details/:id" element={<MovieDetail />} />
       </Routes>
     </BrowserRouter>
   );
