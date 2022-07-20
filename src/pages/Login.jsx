2ea271850928a9a4d541b4ec4240d8e793a624af
ÇETİN <cetinnnarasss@gmail.com>
2022-07-20 20:35:26 +0300

    comt.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,29 +1,31 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const Login = () => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
 
-  const handleSubmit = (e) => {
+  const handleLogin = (e) => {
     e.preventDefault();
+    console.log(email, password);
   };
+
   return (
     <div className="d-flex justify-content-center">
-      <div className="form-image d-none d-md-block">
-        <img src="https://picsum.photos/800/800" alt="random-image" />
+      <div className="form-image d-none d-md-block ">
+        <img src={'https://picsum.photos/800/800'} alt="sample-movie" />
       </div>
       <div className="register-form">
-        <h1 className="form-title display-3">Login</h1>
-        <form id="register" onSubmit={handleSubmit}>
+        <h1 className="form-title display-3 ">Register</h1>
+        <form id="register" onSubmit={handleLogin}>
           <div className="mb-3">
             <label htmlFor="email" className="form-label">
               Email
             </label>
             <input
               type="email"
-              id="email"
               className="form-control"
-              placeholder="Enter your email name"
+              id="email"
+              placeholder="Enter your email adress.."
               required
               onChange={(e) => setEmail(e.target.value)}
             />
@@ -34,15 +36,23 @@ const Login = () => {
             </label>
             <input
               type="password"
-              id="password"
               className="form-control"
-              placeholder="Enter your password name"
+              id="password"
+              placeholder="Enter your password.."
               required
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          <input type="submit" className="btn btn-primary" value="Register" />
+          <div className="link">Forgot Password?</div>
+          <input
+            type="submit"
+            className="btn btn-primary form-control"
+            value="Login"
+          />
         </form>
+        <button className="btn btn-primary form-control ">
+          Continue with Google
+        </button>
       </div>
     </div>
   );
